6d8eec52e6ee8fe5bb06c482d258b044a5abc68a
Joshua Hsieh <hsiehjoshua424@gmail.com>
2026-05-10 18:11:39 -0700

    update test script to accept modes argument

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
-require('./src/fetchStats').fetchStats().then((stats) => {
+const modes = process.argv[2] || '10w 15s 60s';
+
+require('./src/fetchStats').fetchStats(modes).then((stats) => {
+  console.log('Modes:', modes);
   console.log('Result:', JSON.stringify(stats, null, 2));
 }).catch((err) => {
   console.error('Error:', err.message);
